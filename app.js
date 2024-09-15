340aaaeb85da40c12558907c68f29c9a7e485442
Zulfiqar Ali <zulfiqaraliwd@gmail.com>
2024-09-15 15:29:10 +0500

    chapter no 5 completed

---
+++ app.js
@@ -0,0 +1,115 @@
+ //Question no 07------------------------------------------------------
+/*  
+ var price_item1 = +prompt("enter the price of item-1");
+var price_item2 = +prompt("enter the price of item-2");
+var orderQuantity_item1 = +prompt("enter the order quantity of item-1");
+var orderQuantity_item2 = +prompt("enter the order quantity of item-2");
+var shippingCharge = +prompt("enter the shipping charge");
+
+console.log("price of item 1" + price_item1);
+console.log("price of item 2" + price_item2);
+console.log("quantity of item 1" + orderQuantity_item1);
+console.log("quantity of item 2" + orderQuantity_item2);
+console.log("shipping charge" + shippingCharge);
+
+var total_expence = (price_item1 * orderQuantity_item1) + (price_item2 * orderQuantity_item2 ) + shippingCharge;
+console.log(total_expence);
+
+*/
+
+
+
+
+//Question 08---------------------------------------------------------------
+
+/*var english = +prompt("enter the marks of english subject");
+var urdu = +prompt("enter the marks of urdu subject");
+
+
+console.log("marks of english= " + english);
+console.log("marks of urdu = " + urdu);
+
+var totalMarks = 200;
+var obtainMarks = english + urdu;
+
+var persentage = (obtainMarks/totalMarks)*100;
+console.log("persentage" + persentage + "%")
+*/
+
+
+//Question no 09----------------------------------------------------------------
+
+/* var dollar = +prompt("enter the currency of US Dollar");
+var saudi = +prompt("enter the currency of Saudi Riyal");
+
+var pakistan_dollar = dollar * 104 ;
+var pakistan_riyal = saudi * 28;
+
+console.log( dollar + " US Dollar " + pakistan_dollar + " rupees");
+console.log( saudi + " Saudi Riyal " + pakistan_riyal + " rupees");
+
+*/
+
+
+
+
+//Question no 10--------------------------------------------------------------------------
+
+/*
+var addition = +prompt("enter any number you wants");
+var add_5 = addition + 5;
+var mul_10 = add_5 * 10;
+var div_2 = mul_10/2;
+
+console.log(div_2);
+
+*/
+
+
+
+
+
+//Question no 11--------------------------------------------------------------------------
+
+/*var current_year = +prompt("enter the current year");
+var birth_year = +prompt("enter your birth year");
+
+var age = current_year - birth_year;
+console.log("birth year "+ birth_year);
+console.log("current year "+current_year);
+console.log("your age is " + age);
+*/
+
+
+
+//Question no 12-----------------------------------------------------------------------------
+/*
+var r = +prompt("enter the radius of the circle");
+var pie = 3.14;
+var circumference_of_circle = 2 * pie * r;
+
+console.log("radius is " + r);
+console.log("circumference of circle "+ circumference_of_circle)
+console.log("area of the circle " + circumference_of_circle * r + " radian");
+*/
+
+
+
+//Question no 13 ------------------------------------------------------------------------------
+
+
+/*
+var favorite_snack = "nuts";
+var current_age = 22;
+
+var maximum_age = 70;
+var amount_perDay = 2;
+
+var total_snacks = (maximum_age - current_age) * 365 * amount_perDay;
+
+console.log("Maximum age " + maximum_age);
+console.log("current age " + current_age);
+console.log("favorite snack is " + favorite_snack);
+console.log("toyal snack in whole life " + total_snacks);
+
+*/
